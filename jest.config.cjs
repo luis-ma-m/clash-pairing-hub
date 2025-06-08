b8f3891608b36377ee69790d35713f7a93994c6f
Luis Martín Maíllo <luismarmai@gmail.com>
2025-06-08 14:16:27 +0200

    Fix Jest preset

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,12 +1,15 @@
 module.exports = {
-  preset: 'ts-jest/presets/default-esm',
+  preset: 'ts-jest',
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-  transform: {
-    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: 'tsconfig.app.json', useESM: true }]
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.app.json',
+      useESM: true
+    }
   }
 };
